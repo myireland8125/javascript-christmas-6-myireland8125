d2b54c9f812dc884b448e149d08c39de32032ab7
myireland8125 <iloveyouyk88@gmail.com>
2023-11-13 01:50:05 +0900

    [feat] 메뉴판, 뱃지 데이터를 생성한다.

---
+++ src/datas/eventBadge.js
@@ -0,0 +1,8 @@
+const BADGES = {
+  star: { threshold: 5000, label: '별' },
+  tree: { threshold: 10000, label: '트리' },
+  santa: { threshold: 20000, label: '산타' },
+  none: { threshold: 0, label: '없음' },
+};
+
+export default BADGES;
