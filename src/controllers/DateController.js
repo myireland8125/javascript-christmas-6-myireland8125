2dfc83955a9b6d4546722358997be22456d31db2
myireland8125 <iloveyouyk88@gmail.com>
2023-11-13 22:21:20 +0900

    [feat] 날짜의 입력을 다루는 컨트롤러를 생성한다.

---
+++ src/controllers/DateController.js
@@ -0,0 +1,29 @@
+import InputView from '../views/InputView.js';
+import OutputView from '../views/OutputView.js';
+import Date from '../models/Date.js';
+
+class DateController {
+  #date;
+
+  async setPromoteDate() {
+    const { printMessage } = OutputView;
+
+    try {
+      const selectedDate = await this.propmtDate();
+      const selecteNumberDate = Number(selectedDate);
+      this.#date = new Date(selecteNumberDate);
+    } catch (error) {
+      printMessage('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
+      await this.setPromoteDate();
+    }
+  }
+
+  async propmtDate() {
+    return InputView.readDate();
+  }
+
+  getDate() {
+    return this.#date.getDate();
+  }
+}
+export default DateController;
